bc6001b4084a9c0a3a97828f8a57c40929e567cf
sanwhy <10203914why@gmail.com>
2025-01-17 16:45:36 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,7 +42,7 @@ module.exports = async function createConfigAsync() {
       },
       navbar: {
         logo: {
-          alt: '1111111尚宇',
+          alt: '1111111张三',
           src: 'img/logo.webp',
           srcDark: 'img/logo.webp',
         },
@@ -172,7 +172,7 @@ module.exports = async function createConfigAsync() {
             ],
           },
         ],
-        // copyright: `<p><a href="http://beian.miit.gov.cn/" >${beian}</a></p><p>Copyright © 2020 - PRESENT 尚宇 Built with Docusaurus.</p>`,
+        // copyright: `<p><a href="http://beian.miit.gov.cn/" >${beian}</a></p><p>Copyright © 2020 - PRESENT 张三 Built with Docusaurus.</p>`,
         copyright: `本站所有内容遵循<a href="https://creativecommons.org/licenses/by/4.0/deed.zh" >${beian}</a>开源协议，仅限非商业性使用，转载请注明出处</p><p>Copyright © 2022 - PRESENT 尚123宇 Built with Docusaurus.</p>`,
       },
       prism: {
@@ -235,7 +235,7 @@ module.exports = async function createConfigAsync() {
         tagName: 'meta',
         attributes: {
           name: 'description',
-          content: '2222222尚宇的个人博客',
+          content: '2222222张三的个人博客',
         },
       },
     ],
@@ -278,7 +278,7 @@ module.exports = async function createConfigAsync() {
           editUrl: ({ locale, blogDirPath, blogPath, permalink }) =>
             `https://github.com/disnox/blog/edit/main/${blogDirPath}/${blogPath}`,
           editLocalizedFiles: false,
-          blogDescription: '333333尚宇的个人博客',
+          blogDescription: '333333张三的个人博客',
           blogSidebarCount: 10,
           blogSidebarTitle: 'Blogs',
           postsPerPage: 10,
@@ -287,8 +287,8 @@ module.exports = async function createConfigAsync() {
             defaultReadingTime({ content, options: { wordsPerMinute: 300 } }),
           feedOptions: {
             type: 'all',
-            title: '444尚宇',
-            copyright: `Copyright © ${new Date().getFullYear()} 尚宇 Built with Docusaurus.<p><a href="http://beian.miit.gov.cn/" class="footer_lin">${beian}</a></p>`,
+            title: '444张三',
+            copyright: `Copyright © ${new Date().getFullYear()} 张三 Built with Docusaurus.<p><a href="http://beian.miit.gov.cn/" class="footer_lin">${beian}</a></p>`,
           },
         },
       ],
